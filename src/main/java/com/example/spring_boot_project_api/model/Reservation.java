e9e73de63c2628e66ab904fddf85aab1760ecdfb
Sengly Sea <optionalsa@gmail.com>
2026-08-25 11:04:11 +0700

    remove blank

--- src/main/java/com/example/spring_boot_project_api/model/Reservation.java
+++ src/main/java/com/example/spring_boot_project_api/model/Reservation.java
@@ -19,7 +19,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
